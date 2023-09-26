695c9c9329dccf89333519a24c94ffa0487cf376
sounny <sounny@gmail.com>
2023-09-26 16:45:10 -0400

    Create main.js

---
+++ main.js
@@ -0,0 +1,22 @@
+// Initialize the map and set its initial view to some geographic coordinates and a zoom level
+var mymap = L.map('mapid').setView([51.505, -0.09], 13);
+
+// Add a tile layer to the map
+Lvar OpenStreetMap_Mapnik = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+	maxZoom: 19,
+	attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(mymap);
+
+// Add a marker, circle, and polygon to the map
+var marker = L.marker([51.5, -0.09]).addTo(mymap);
+var circle = L.circle([51.508, -0.11], {
+    color: 'red',
+    fillColor: '#f03',
+    fillOpacity: 0.5,
+    radius: 500
+}).addTo(mymap);
+var polygon = L.polygon([
+    [51.509, -0.08],
+    [51.503, -0.06],
+    [51.51, -0.047]
+]).addTo(mymap);
